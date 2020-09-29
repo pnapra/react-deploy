4e6ea33fc4a4f6757544e2f0bbf01b45fb3e5fa1
Arpan <Arpan@Arpans-MBP.attlocal.net>
2020-09-29 16:43:29 -0400

    Updates

---
+++ precache-manifest.d522fce81bc75fac4a675c69a63dfe38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9bf8df8cbcb42ce3032026a56a31067",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "a2b41f1d7785c85cc501",
+    "url": "/react-deploy/static/css/main.1f61901d.chunk.css"
+  },
+  {
+    "revision": "e0aab9fc98bcbf8e799e",
+    "url": "/react-deploy/static/js/2.7e1dd65a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.7e1dd65a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b41f1d7785c85cc501",
+    "url": "/react-deploy/static/js/main.4c30a100.chunk.js"
+  },
+  {
+    "revision": "f8bf1ed71cd8aa5446fb",
+    "url": "/react-deploy/static/js/runtime-main.9b438990.js"
+  }
+]);
